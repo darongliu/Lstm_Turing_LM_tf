6e3c9c25ffa147a232e71a1e55283bc6aee10573
darongliu <b01901079@ntu.edu.tw>
2017-02-05 00:04:38 +0800

    finish all_model

---
+++ all_model_prototype/model.py
@@ -0,0 +1,166 @@
+import tensorflow as tf
+
+def inference(input_x, embedding_dim, lstm_hidden_dim_1, lstm_hidden_dim_2=None, vocab_size, dropout=None) :
+    """
+    Args:
+    input_x: 2D tensor batch_size X time_step
+    embedding_dim: embedding dimension
+    hidden_unit_list: list of the hidden unit size
+    lstm_hidden_dim_1: the dimension of the hidden unit of the bottom lstm 
+    lstm_hidden_dim_2(optional): the dimension of the hidden unit of the top lstm
+
+    Returns:
+
+    """
+
+    with tf.name_scope('embedding'):
+        init_width = 0.5 / embedding_dim
+        emb = tf.Variable(
+            tf.random_uniform(
+            [vocab_size, embedding_dim], -init_width, init_width),
+            name="emb")
+        input_emb = tf.nn.embedding_lookup(emb, input_x)
+
+    with tf.name_scope('recurrent_layer1'):
+        cell = tf.nn.rnn_cell.LSTMCell(lstm_hidden_dim_1, state_is_tuple=True)
+        if dropout:
+            cell = tf.nn.rnn_cell.DropoutWrapper(cell, output_keep_prob=dropout)
+        initial_state_vector = tf.get_variable('initial_state_vector', [1, lstm_hidden_dim_1])
+        initial_state = tf.tile(initial_state_vector, [tf.shape(input_x)[0], 1])
+        lstm1_outputs, final_state = tf.nn.dynamic_rnn(cell, input_emb, initial_state=initial_state)
+        #lstm1_outputs: [batch_size, num_steps, state_size]
+
+    with tf.name_scope('attention_layer'):
+    #using scan
+    #TODO
+        memory = tf.concat([initial_state,lstm1_outputs],1)
+        #memory: [batch_size, num_steps, state_size]
+        W_key = tf.get_variable('W_key', [lstm_hidden_dim_1, lstm_hidden_dim_1])
+        b_key = tf.get_variable('b_key', [lstm_hidden_dim_1], initializer=tf.constant_initializer(0.0))
+
+        W_memory_selection_1 = tf.get_variable('W_memory_selection_1', [lstm_hidden_dim_1, lstm_hidden_dim_1])
+        b_memory_selection_1 = tf.get_variable('b_memory_selection_1', [lstm_hidden_dim_1], initializer=tf.constant_initializer(0.0))
+
+        W_memory_selection_2 = tf.get_variable('W_memory_selection_2', [lstm_hidden_dim_1, lstm_hidden_dim_1])
+        b_memory_selection_2 = tf.get_variable('b_memory_selection_2', [lstm_hidden_dim_1], initializer=tf.constant_initializer(0.0))
+
+        def step(_,time_step):
+            """
+            args:
+            timestep:from 0 to the last time step
+            """
+            current_hidden = memory[:,time_step+1,:]
+            previous_hidden = memory[:,:time_step+1,:]
+            #previous_hidden: [batch_size, num_steps, state_size]
+
+            memory_selection_1 = tf.sigmoid(tf.matmul(current_hidden,W_memory_selection_1) + b_memory_selection_1)
+            memory_selection_2 = tf.sigmoid(tf.matmul(current_hidden,W_memory_selection_2) + b_memory_selection_2)
+            #memory selection: [batch_size, state_size]
+            key = tf.matmul(current_hidden,W_key) + b_key
+            #key: [batch_size, state_size]
+
+            memory_selection_1_as_matrices = tf.expand_dims(memory_selection_1,1)
+            similarity_temp = tf.multiply(previous_hidden,memory_selection_1_as_matrices) 
+            key_as_matrices = tf.expand_dims(key,2)
+            similarity = tf.batch_matmul(similarity_temp, key_as_matrices)
+            #similarity = [batch_size, num_steps]
+            weight = tf.nn.softmax(similarity)
+            entropy_temp = -tf.multiply(weight,tf.log(weight))
+            entropy = tf.reduce_sum(entropy_temp,1)
+
+            weight_pad_length = tf.shape(input_x)[1] - time_step - 1
+            weight_pad = tf.concat([weight,tf.zeros([tf.shape(input_x)[0],weight_pad_length])],1) #for weight visualization
+
+            memory_selection_2_as_matrices = tf.expand_dims(memory_selection_2,1)
+            attention_temp = tf.multiply(previous_hidden,memory_selection_2_as_matrices) 
+            weight_as_matrics = tf.expand_dims(weight,1)
+            attention = tf.batch_matmul(weight_as_matrics, previous_hidden)
+            attention = tf.squeeze(attention,[1])
+
+            return attention, weight_pad, entropy
+
+        time_step_sequence = tf.range(0,tf.shape(input_x)[2])
+        initializer = [tf.zeros([tf.shape(input_x)[0], lstm_hidden_dim_1]),
+                       tf.zeros_like(input_x),
+                       tf.zeros([tf.shape(input_x)[0],])]
+
+        att_outputs, weight_outputs, entropy_outputs = \
+            tf.scan(step,time_step_sequence,initializer=initializer)
+
+    with th.name_scope('merge_layer'):
+        if merge_mode == 'concat':
+            att_lstm_outputs = tf.concat([lstm1_outputs,att_outputs],2)
+        else if merge_mode == 'matrix':
+            merge_matrix = tf.Variable(
+                tf.random_uniform(
+                [lstm_hidden_dim_1, lstm_hidden_dim_1], -1, 1),
+                name="merge_matrix")
+            att_lstm_outputs = tf.matmul(lstm1_outputs,att_outputs) + lstm1_outputs
+        else if merge_mode == 'alpha':
+            merge_alpha = tf.Variable(
+                1,
+                name="merge_alpha")
+            att_lstm_outputs = merge_alpha*lstm1_outputs + 
+                                (1-merge_alpha)*att_outputs
+        else:
+            print("unknown merge mode, using concat")
+            att_lstm_outputs = tf.concat([lstm1_outputs,att_outputs],2)
+
+    with tf.name_scope('recurrent_layer2'):
+        cell = tf.nn.rnn_cell.LSTMCell(lstm_hidden_dim_2, state_is_tuple=True)
+        if dropout:
+            cell = tf.nn.rnn_cell.DropoutWrapper(cell, output_keep_prob=dropout)
+        initial_state_vector = tf.get_variable('initial_state_vector', [1, lstm_hidden_dim_2])
+        initial_state = tf.tile(initial_state_vector, [tf.shape(input_x)[0], 1])
+        lstm2_outputs, final_state = tf.nn.dynamic_rnn(cell, att_lstm_outputs, initial_state=initial_state)
+        att_lstm_outputs = lstm2_outputs
+
+    with tf.name_scope('output_lstm_linear'):
+        W = tf.get_variable('W', [lstm_hidden_dim_2, vocab_size],name="w")
+        b = tf.get_variable('b', [vocab_size], initializer=tf.constant_initializer(0.0),name="b")
+
+        logits = tf.matmul(att_lstm_outputs, W) + b
+
+    #with tf.name_scope('entropy_loss') :
+    #dropout, pretrain
+    #add pretrain_param, output_linear_param
+
+    return logits
+
+def loss(logits, labels, entropy=None, entropy_reg=0) :
+    """
+    args:
+        logits: [batch_size, num_steps, vocab_size]
+        labels: [batch_size, num_steps]
+
+    return :
+        label_loss: for calculating perplexity
+        loss: for training
+
+    evaluate perplexity: exp(average of the entropy per word)
+    """
+    cross_entropy_result = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits)
+    label_loss = tf.reduce_sum(cross_entropy_result)
+    #devide vocab size
+    loss = tf.reduce_mean(cross_entropy_result)
+    if entropy :
+        loss = label_loss + entropy_reg*entropy
+
+    return label_loss, loss
+
+def training(loss, learning_rate) :
+    # Add a scalar summary for the snapshot loss.
+    tf.summary.scalar('loss', loss)
+    # Create the gradient descent optimizer with the given learning rate.
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    # Create a variable to track the global step.
+    global_step = tf.Variable(0, name='global_step', trainable=False)
+    # Use the optimizer to apply the gradients that minimize the loss
+    # (and also increment the global step counter) as a single training step.
+    train_op = optimizer.minimize(loss, global_step=global_step)
+    return train_op
+
+
+
+
+
---
+++ all_model_prototype/run.py
@@ -0,0 +1,257 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import time
+import argparse
+
+import numpy as np
+import tensorflow as tf
+
+import reader
+from model import *
+
+def parsing_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mode', type=str, default='train',
+                        help='train or test')
+    parser.add_argument('--init_from', type=str, default='',
+                        help='init model path')
+    parser.add_argument('--init_method', type=str, default='lstm',
+                        help='lstm/att init from lstm or full model')
+
+    parser.add_argument('--data_dir', type=str, default='data/',
+                        help='data directory containing train valid test data')
+    parser.add_argument('--log_file', type=str, default='logs',
+                        help='directory containing tensorboard logs')
+    parser.add_argument('--save', type=str, default='save',
+                        help='directory to store checkpointed models')
+    parser.add_argument('--load', type=str, default=None,
+                        help="continue training from saved model at this path. Path must contain files saved by previous training process")
+    parser.add_argument('--att_file', type=str, default='save',
+                        help='file storing attention weights for analysis')
+
+    parser.add_argument('--rnn_size', type=int, default=200,
+                        help='size of LSTM internal state')
+    parser.add_argument('--emb_size', type=int, default=200,
+                        help='word embedding size')
+    parser.add_argument('--num_layers', type=int, default=1,
+                        help='number of layers in the RNN')
+
+    parser.add_argument('--batch_size', type=int, default=20,
+                        help='minibatch size')
+    parser.add_argument('--max_seq_length', type=int, default=40,
+                        help='max number of timesteps to unroll during BPTT')
+    parser.add_argument('--min_seq_length', type=int, default=15,
+                        help='min number of timesteps to unroll during BPTT')
+
+    parser.add_argument('--max_epochs', type=int, default=50,
+    help='number of full passes through the training data')
+    parser.add_argument('--dropout', type=int, default=0,
+                        help='dropout for regularization, used after each RNN hidden layer. 0 = no dropout')
+    parser.add_argument('--max_grad_norm', type=float, default=5.,
+                        help='clip gradients at this value')
+    parser.add_argument('--entropy_reg', type=float, default=0.1,
+                        help='entropy regulizar')
+
+    parser.add_argument('--learning_rate', type=float, default=1.0,
+                        help='learning rate')
+    parser.add_argument('--decay_rate', type=float, default=0.5,
+                        help='decay rate')
+    parser.add_argument('--learning_rate_decay_after', type=int, default=4,
+                        help='in number of epochs, when to start decaying the learning rate')
+
+    parser.add_argument('--gpu_id', type=float, default=0.666,
+                        help='% of gpu memory to be allocated to this process. Default is 66.6%')
+    parser.add_argument('--print_every', type=int, default=200,
+                        help='how many steps/minibatches between printing out the loss')
+    parser.add_argument('--seed', type=int, default=40,
+                        help='random number generator seed')
+
+    args = parser.parse_args()
+    return args
+    
+def run_epoch_training(sess, all_op, data, lr):
+    start_time = time.time()
+    nbatch = data.get_batch_number()
+    total_words_num = 0
+    total_cost = 0
+
+    fetches = {}
+    fetches["train_op"] = all_op["train_op"]
+    fetches['label_loss'] = all_op['label_loss_op']
+
+    for idx in range(nbatch):
+        x, y = data.get_data(idx)
+
+        feed_dict = {all_op['input_data']:x, all_op['targets']:y, all_op['learning_rate_op']:lr}
+
+        result = sess.run(fetches,feed_dict=feed_dict)
+        total_cost += result['label_loss']
+        total_words_num += x.size
+
+        print idx+1, '/', 'nbatch: ', 'perplexity: ', np.exp(result['label_loss']/x.size)
+
+    total_perplexity = np.exp(total_cost/total_words_num)
+    print 'training perplexity in this epoch: ', total_perplexity
+    print 'epoch training time', (time.time() - start_time)
+
+    return total_perplexity
+
+def evaluating(sess, all_op, data):
+    nbatch = data.get_batch_number()
+    total_words_num = 0
+    total_cost = 0
+    for idx in range(nbatch):
+        x, y = data.get_data(idx)
+
+        feed_dict = {all_op['input_data']:x, all_op['targets']:y}
+        label_loss_op = all_op['label_loss_op']
+
+        cost = sess.run(label_loss_op,feed_dict=feed_dict)
+        total_cost += cost
+        total_words_num += x.size
+
+    perplexity = np.exp(total_cost/total_words_num)
+    print 'evaluating perplexity: ', perplexity
+
+    return perplexity
+
+def train(args):
+    #read data
+    train_data = reader.data(data_dir=args.data_dir, 
+                             batch_size=args.batch_size, 
+                             min_seq_length=args.min_seq_length, 
+                             max_seq_length=args.max_seq_length,
+                             min_count=args.min_count)
+    train_data.load('train')
+    valid_data = reader.data(data_dir=args.data_dir, 
+                             batch_size=args.batch_size, 
+                             min_seq_length=args.min_seq_length, 
+                             max_seq_length=args.max_seq_length,
+                             min_count=args.min_count)
+    valid_data.load('valid')
+    test_data = reader.data(data_dir=args.data_dir, 
+                             batch_size=args.batch_size, 
+                             min_seq_length=args.min_seq_length, 
+                             max_seq_length=args.max_seq_length,
+                             min_count=args.min_count)
+    test_data.load('test')
+
+    #load model
+    if args.init_from:
+        if is not os.path.isfile(args.init_from):
+            print 'init file not found'
+            os.exit()
+
+    #training ops
+    input_data = tf.placeholder(tf.int32, [None, None])
+    targets    = tf.placeholder(tf.int32, [None, None])
+    learning_rate_op = tf.placeholder(tf.float32, [])
+    #build model
+    vocab_size=train_data.vocab_size
+    predict, pretrain_param, output_linear_param = model.inference(input_data, 
+                                                                   embedding_dim=args.emb_size
+                                                                   lstm_hidden_dim_1=args.rnn_size
+                                                                   vocab_size=vocab_size)
+
+    label_loss_op, total_loss_op = model.loss(targets, predict, entropy=True, entropy_reg=args.entropy_reg)
+    train_op = model.training(total_loss_op, learning_rate_op)
+
+    all_op = {'input_data':input_data, 
+              'targets':targets
+              'learning_rate_op':learning_rate_op
+              'label_loss_op':label_loss_op
+              'train_op':train_op}
+
+
+    #pretrain
+    if args.init_method == 'lstm':
+        lstm_linear_w = tf.variable(tf.random_uniform([args.rnn_size, vocab_size], 0, 0),name='output_lstm_linear/w')
+        lstm_linear_b = tf.variable(tf.random_uniform([vocab_size], 0, 0),name='output_lstm_linear/b')
+        init_att_w = output_linear_param['w'].assign(tf.concat([lstm_linear_w,lstm_linear_w],0))
+        init_att_b = output_linear_param['b'].assign(lstm_linear_b)
+        saver_restore = tf.train.Saver(pretrain_param)
+    else:
+        saver_restore = tf.train.Saver()
+    init = tf.initialize_all_variables()
+    saver_save = tf.train.Saver()
+    global_step = tf.Variable(0,name='global_step',trainable=False)
+
+    with tf.Session() as sess:
+        sess.run(init)
+        if args.init_method == 'lstm':
+            saver_restore.restore(sess, args.init_from)
+            sess.rum(init_att_w)
+            sess.rum(init_att_b)
+        else:
+            saver_restore.restore(sess, args.init_from)
+
+        #training
+        best_val_perplexity = np.inf
+
+        for i in range(args.max_epochs):
+            lr_decay = args.decay_rate ** max(i + 1 - args.learning_rate_decay_after, 0.0)
+            learning_rate = args.learning_rate * lr_decay
+            print("Epoch: %d Learning rate: %.3f" % (i + 1, learning_rate))
+
+            #training
+            training_perplexity = run_epoch_training(sess, all_op, train_data, learning_rate)
+            print("Epoch: %d Train Perplexity: %.3f" % (i + 1, training_perplexity))
+
+            #validation
+            val_perplexity = evaluating(sess, all_op, valid_data)
+            print("Epoch: %d validation Perplexity: %.3f" % (i + 1, val_perplexity))
+
+            #peeking testing
+            test_perplexity = evaluating(sess, all_op, test_data)
+            print("Epoch: %d peeking testing Perplexity: %.3f" % (i + 1, test_perplexity))
+
+            if val_perplexity < best_val_perplexity :
+                best_val_perplexity = val_perplexity
+                #save
+                saver_save.save(session, args.save, global_step=global_step)
+
+def test(args):
+    test_data = reader.data(data_dir=args.data_dir, 
+                             batch_size=args.batch_size, 
+                             min_seq_length=args.min_seq_length, 
+                             max_seq_length=args.max_seq_length)
+    test_data.load('test')
+    #build model
+    input_data = tf.placeholder(tf.int32, [None, None])
+    targets    = tf.placeholder(tf.int32, [None, None])
+    learning_rate_op = tf.placeholder(tf.float32, [])
+    #build model
+    vocab_size=test_data.vocab_size
+    predict, pretrain_param, output_linear_param = model.inference(input_data, 
+                                                                   embedding_dim=args.emb_size
+                                                                   lstm_hidden_dim_1=args.rnn_size
+                                                                   vocab_size=vocab_size)
+
+    label_loss_op, total_loss_op = model.loss(targets, predict, entropy=True, entropy_reg=args.entropy_reg)
+    train_op = model.training(total_loss_op, learning_rate_op)
+
+    all_op = {'input_data':input_data, 
+              'targets':targets
+              'learning_rate_op':learning_rate_op
+              'label_loss_op':label_loss_op
+              'train_op':train_op}
+
+    #load model
+    init = tf.initialize_all_variables()
+    with tf.Session() as sess:
+        sess.run(init)
+        saver.restore(sess, args.init_from)
+
+        test_perplexity = evaluating(sess, all_op, test_data)
+        print ("Testing Perplexity: %.3f" % (test_perplexity))
+
+if __name__ == "__main__":
+    args = parsing_args()
+    if args.mode == 'train':
+        train(args)
+    else :
+        test(args)
+
+
