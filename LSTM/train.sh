ef61ac34fcc7b14fac8fb1decfcc2f3143c63006
ShunPo <alex82528@hotmail.com.tw>
2017-02-14 23:45:51 +0800

    add store perplexity in training mechanism

--- LSTM/train.sh
+++ LSTM/train.sh
@@ -6,6 +6,7 @@ word_vector_path='../data/word2vec/GoogleNews-vectors-negative300.bin'
 
 data_dir='../data/PTB_data'
 save='./model/'
+model_result='./result/'
 #att_file
 
 rnn_size=300
@@ -28,4 +29,4 @@ learning_rate_decay_after=15
 #gpu_id
 print_every=100
 
-python ./run.py --mode $mode --word_vector_path $word_vector_path --data_dir $data_dir --save $save --rnn_size $rnn_size --emb_size $emb_size --num_layers $num_layers --batch_size $batch_size --max_seq_length $max_seq_length --min_seq_length $min_seq_length --max_epochs $max_epochs --dropout $dropout --max_grad_norm $max_grad_norm --learning_rate $learning_rate --decay_rate $decay_rate --learning_rate_decay_after $learning_rate_decay_after --print_every $print_every
+python ./run.py --mode $mode --word_vector_path $word_vector_path --data_dir $data_dir --save $save --model_result $model_result --rnn_size $rnn_size --emb_size $emb_size --num_layers $num_layers --batch_size $batch_size --max_seq_length $max_seq_length --min_seq_length $min_seq_length --max_epochs $max_epochs --dropout $dropout --max_grad_norm $max_grad_norm --learning_rate $learning_rate --decay_rate $decay_rate --learning_rate_decay_after $learning_rate_decay_after --print_every $print_every
