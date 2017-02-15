02667e47532e462710cb3576f484f5e861cdc00e
ShunPo <alex82528@hotmail.com.tw>
2017-02-15 12:48:33 +0800

    modified

--- attention_LSTM/train.sh
+++ attention_LSTM/train.sh
@@ -25,8 +25,9 @@ max_grad_norm=5
 learning_rate=1
 decay_rate=0.8
 learning_rate_decay_after=30
+init_scale=0.06
 
-#gpu_id
+gpu_id=1
 print_every=100
 
-python ./run.py --mode $mode --word_vector_path $word_vector_path --data_dir $data_dir --save $save --model_result model_result --rnn_size $rnn_size --emb_size $emb_size --num_layers $num_layers --batch_size $batch_size --max_seq_length $max_seq_length --min_seq_length $min_seq_length --max_epochs $max_epochs --dropout $dropout --max_grad_norm $max_grad_norm --learning_rate $learning_rate --decay_rate $decay_rate --learning_rate_decay_after $learning_rate_decay_after --print_every $print_every
+CUDA_VISIBLE_DEVICES=$gpu_id python ./run.py --mode $mode --word_vector_path $word_vector_path --data_dir $data_dir --save $save --model_result model_result --rnn_size $rnn_size --emb_size $emb_size --num_layers $num_layers --batch_size $batch_size --max_seq_length $max_seq_length --min_seq_length $min_seq_length --max_epochs $max_epochs --dropout $dropout --max_grad_norm $max_grad_norm --learning_rate $learning_rate --decay_rate $decay_rate --learning_rate_decay_after $learning_rate_decay_after --init_scale $init_scale --print_every $print_every
